4bd309e1c62fb54e959d9ddb109870b9b1c9f0a2
Nathan Lee <nslee333@gmail.com>
2021-10-27 18:26:21 -0700

    Commit

--- MyContract.sol
+++ MyContract.sol
@@ -1,12 +1,35 @@
-pragma solidity ^0.4.24;
+pragma solidity ^0.4.24; 
 
 
 contract myContract {
-    string value;
+    string value; // This is the state variable.
 
-    function get() public returns(string) {
+
+    // A function to read the value.
+    function get() public view returns(string) {
         return value;
     }
+
+
+    // A function to write the value.
+    function set(string _value) public {
+        value = _value;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
 
 
