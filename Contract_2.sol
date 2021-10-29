bb6167cdac1b4d5b0c22e0d798570b8f0b018eaa
Nathan Lee <nslee333@gmail.com>
2021-10-28 22:02:01 -0700

    Commit

--- Contract_2.sol
+++ Contract_2.sol
@@ -4,18 +4,5 @@ pragma solidity ^0.5.1;
 contract myContract {
     
     // This is the state variable.
-    string public value; 
-
-
-    // This is the constructor function.
-    constructor() public {
-        value = "myValue";
-    }
-
-
-    // A function to write the value of the state variable.
-    function setValue(string memory _value) public {
-        value = _value;
-    }
-
+    string public constant stringValue = "myValue";
 }
