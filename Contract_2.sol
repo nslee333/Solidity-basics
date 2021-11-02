c0288417c19f7edaccea0454adc402577c2afafe
Nathan Lee <nslee333@gmail.com>
2021-11-02 14:31:50 -0700

    Commit

--- Contract_2.sol
+++ Contract_2.sol
@@ -2,9 +2,8 @@ pragma solidity ^0.5.1;
 
 
 contract myContract {
-    
-    // This is the state variable.
-    string public stringValue = "myString";
-    bool public myBool = true;
-    int public myInt = -1;
+
+
+
+
 }
