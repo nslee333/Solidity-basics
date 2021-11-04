10cb90f9194ab4d4c7369d4bfc029ad428043063
Nathan Lee <nslee333@gmail.com>
2021-11-03 17:52:01 -0700

    Commit

--- Contract_2.sol
+++ Contract_2.sol
@@ -3,9 +3,14 @@ pragma solidity ^0.5.1;
 
 contract myContract {
     uint256 public peopleCount = 0;
-    
     mapping(uint => Person) public people; 
 
+    address owner;
+
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
 
     struct Person {
         uint _id;
@@ -13,9 +18,18 @@ contract myContract {
         string _lastName;
     }
 
-    function addPerson(string memory _firstName, string memory _lastName) public {
-        peopleCount += 1;
+    constructor() public {
+        owner = msg.sender;
+    }
+
+
+    function addPerson(string memory _firstName, string memory _lastName) public onlyOwner {
+        incrementCount();
         people[peopleCount] = Person(peopleCount, _firstName, _lastName);
 
+    }
+
+    function incrementCount() internal {
+        peopleCount += 1;
     }
 }
