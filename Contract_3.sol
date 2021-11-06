3446b046ed2beb518cdc9eba41a9aa3ac9d07fd0
Nathan Lee <nslee333@gmail.com>
2021-11-05 19:07:32 -0700

    Commit

--- Contract_3.sol
+++ Contract_3.sol
@@ -2,15 +2,21 @@ pragma solidity 0.5.1;
 
 
 contract myContract {
+    mapping(address => uint256) public balances;
+    // mapping, user address which is key, and the value is the 
+    // balance of the account, public visibility and the mapping 
+    // variable name is balances.
+
+    
 
 
 
+    function buyToken() {
+        // Buy a token.
+        balances[msg.sender] += 1;
+        // Send ether to the wallet.
 
 
 
-
-
-
-
-
+    }
 }
