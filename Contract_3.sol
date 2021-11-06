2ac360f9865b1aa72e247019fd1821e8f59b8a5f
Nathan Lee <nslee333@gmail.com>
2021-11-05 19:39:05 -0700

    Commit

--- Contract_3.sol
+++ Contract_3.sol
@@ -6,14 +6,17 @@ contract myContract {
     // mapping, user address which is key, and the value is the 
     // balance of the account, public visibility and the mapping 
     // variable name is balances.
+    address payable wallet;
 
-    
+    constructor(address payable _wallet) public {
+        wallet = _wallet;
+    }
 
 
-
-    function buyToken() {
+    function buyToken() public payable {
         // Buy a token.
         balances[msg.sender] += 1;
+        wallet.transfer(msg.value);
         // Send ether to the wallet.
 
 
