437131cb3e9fabecb10112aa3a9eed3f62cff1a6
Nathan Lee <nslee333@gmail.com>
2021-11-12 23:22:38 -0800

    Commit

--- Contract_4.sol
+++ Contract_4.sol
@@ -3,36 +3,16 @@
 pragma solidity 0.5.1;
 
 contract ERC20Token {
-    
     string public name;
-
     mapping(address => uint256) public balances;
 
     function mint() public payable {
         balances[tx.origin] ++;
 
-    constructor(string memory _name) public {
+    constructor(string memory name) public {
         name = _name;
     }
     function mint() public {
         balances[tx.origin] ++;
     }
-
-    }
-
-
-
-contract myToken is ERC20Token {
-    bytes32 public _symbol;
-
-    constructor(
-        bytes32 memory _name, 
-        bytes32 memory _symbol
-    ) 
-        ERC20Token(_name)
-    public {
-        symbol = _symbol;
-    }
 }
-
-
