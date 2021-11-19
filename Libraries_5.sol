9b6da861674a5549bc201c657e9d6cd5588af2fd
Nathan Lee <nslee333@gmail.com>
2021-11-18 19:32:53 -0800

    Commit

--- Libraries_5.sol
+++ Libraries_5.sol
@@ -1,5 +1,20 @@
 pragma solidity ^0.5.1;
 
+library Math {
+    function divide(uint256 a, uint256 b) internal pure returns (uint256) {
+        require(b > 0)
+        uint256 c = a / b;
+        return c;
+//  
+    }
+
+}
+
+
 contract myContract {
-    // DRY - "Don't repeat yourself".
+    uint256 public value;
+
+    function calculate(uint _value1, uint _value2) public {
+        value = Math.divide(_value1, _value2);
+    }
 }
