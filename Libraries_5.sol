e0140035421fcc1b8dc671547b3467ebb6b41e1b
Nathan Lee <nslee333@gmail.com>
2021-11-18 20:36:19 -0800

    Commit

--- Libraries_5.sol
+++ Libraries_5.sol
@@ -4,9 +4,15 @@ pragma solidity ^0.5.1;
 import "./Math.sol";
 
 contract myContract {
+    
+    using SafeMath for uint256;
+
+
+    
     uint256 public value;
 
     function calculate(uint _value1, uint _value2) public {
         value = Math.divide(_value1, _value2);
+        // value = _value1.div(_value2);
     }
 }
